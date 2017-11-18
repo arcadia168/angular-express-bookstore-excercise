b0415d0cb875bd4f46c95578ade377207b610406
Erin Eland <erin@erineland.com>
2017-11-19 00:48:24 +0100

    made enter key trigger search

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,10 +1,7 @@
 // public/js/controllers/MainCtrl.js
 angular.module('MainCtrl', ['BookService']).controller('MainController', function ($scope, Book) {
 
-    $scope.tagline = 'To the moon and back!';
-
     Book.all().then(function (books) {
-        //console.log(JSON.stringify(books));
         console.log(books);
         $scope.books = books.data;
         $scope.searchResults = books.data;
